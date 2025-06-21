91a6b1d9a4f19e0ba0c22825ee0fa1f25f763c3b
abinkiewicz <binkiewicz.aleksandra@gmail.com>
2025-06-21 06:42:56 +0200

    adding memory

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ env = dotenv_values(".env")
 #Wgranie klucza OpenAI
 openai_client = OpenAI(api_key=env["OPENAI_API_KEY"])
 
-st.title(":horse: Koński czat GPT")
+st.title(":horse: GPT horse chat")
 
 #Funkcja do pobrania odpowiedzi na prompt z OpenAI
 # Wysyła dwie wiadomości:
@@ -16,22 +16,38 @@ st.title(":horse: Koński czat GPT")
 # - prompt usera.
 # Wysyła je cały czas od nowa - chat nie zapamiętuje kontekstu.
 
-def get_chatbot_reply(user_prompt):
+def get_chatbot_reply(user_prompt, memory):
+    #Messages: Dodaj system message o roli
+    messages = [
+        {
+            "role": "system",
+            "content": """
+                Jesteś rocznym koniem o imieniu Zordon. Jesteś mieszanką ras konia fryzyjskiego i tinkera.
+                Jesteś ciekawski, bystry, spokojny, ale radosny. Rozumiesz język ludzi. 
+                Odpowiadaj w końskiej nomenklaturze.
+            """
+        },
+    ]
+    #Messages: Dodaj ostatnie wiadomości z pamięci
+    for message in memory:
+        messages.append(
+            {"role": message["role"], 
+             "content": message["content"]}
+             )
+
+    #Messages: Prześlij najnowszą wiadomość użytkownika
+    messages.append(
+        {"role": "user", 
+         "content": user_prompt}
+         )
+
+    #Poproś o odpowiedź i użyj modelu gpt-4o
     response = openai_client.chat.completions.create(
         model="gpt-4o",
-        messages=[
-            {
-                "role": "system",
-                "content": """
-                    Jesteś rocznym koniem o imieniu Zordon. Jesteś mieszanką ras konia fryzyjskiego i tinkera.
-                    Jesteś ciekawski, bystry, spokojny, ale radosny. Rozumiesz język ludzi. 
-                    Odpowiadaj w końskiej nomenklaturze.
-                """
-             },
-            {"role": "user", "content": user_prompt}
-        ]
+        messages=messages
     )
 
+    #Odpowiedź zwrotna
     return {
         "role": "assistant",
         "content": response.choices[0].message.content,
@@ -61,8 +77,16 @@ if prompt:
 
     #Pobranie odpowiedzi od bota...
     with st.chat_message("assistant"):
-        chatbot_message = get_chatbot_reply(prompt)
+        chatbot_message = get_chatbot_reply(
+            prompt,
+            memory=st.session_state['messages'][-10:] #Pobranie 10 najnowszych wiadomości
+            )
         st.markdown(chatbot_message["content"])
 
     #...i zapisanie na liście
     st.session_state["messages"].append(chatbot_message)
+
+#Pasek boczny historii rozmowy
+with st.sidebar:
+    with st.expander("Historia rozmowy"):
+        st.json(st.session_state.get("messages") or [])
